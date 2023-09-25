bcff9c02caa0236256aa0d6fb794f3ab7a9adf97
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-09-25 14:15:45 -0400

    done by Navruzbek

--- basic_func09.py
+++ basic_func09.py
@@ -1,5 +1,7 @@
 # Create a function called main.
-
+def a(name):
 # Create function arguments a.
-
+    answer=200
 # decrease the value of a to one and return
+    return answer-1
+print(a("answer"))
